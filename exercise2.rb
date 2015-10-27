43600013e67e0fc05430b08ffe2e549eeb1b6094
stephthi <nstephanie2@gmail.com>
2015-10-27 14:40:30 -0400

    Add second line

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,11 @@
 # Use puts to print the answer onscreen.
 
 puts 55 * 0.15 
+
+# Try adding a string and an integer. What happens? 
+# Find a way to convert the integer so that it works and use 
+# puts to print the answer onscreen.
+
+number = 3
+puts "Number equates to #{number}."
+
