ef0ffe02f53af4764f4c5efde1a437a69cb53f6c
stephthi <nstephanie2@gmail.com>
2015-10-27 15:03:52 -0400

    Age math

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+# Asking age
+
+puts "What year were you born?"
+year = gets.chomp
+math = 2015 - year.to_i
+puts "You are #{math} years old!"	
+
+
